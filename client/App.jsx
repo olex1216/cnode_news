7dac15f28d4a67768bd829f4b75078d8620f6321
olex1216 <olex1216@gmail.com>
2018-04-07 17:41:26 +0800

    init

---
+++ client/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// export default class App extends React.Component {
+//   render() {
+//     return (
+//       <div>This is React asaApp</div>
+//     );
+//   }
+// }
+const App = () => <div>This is React asaApp</div>
+
+export default App;
