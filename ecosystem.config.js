2fbe226ec82cbb2aa18279f39f2bb8a5ca98a771
olex1216 <olex1216@gmail.com>
2018-05-16 01:17:49 +0800

    pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,52 +1,22 @@
-module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
-  apps : [
-
-    // First application
+{
+  "apps": [
     {
-      name      : 'API',
-      script    : 'app.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
+      "name": "jnode",
+      "script": "./server/server.js",
+      "env_production": {
+        "NODE_ENV": "production",
+        "HOST": "localhost"
       }
-    },
-
-    // Second application
-    {
-      name      : 'WEB',
-      script    : 'web.js'
     }
   ],
-
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
-    },
-    dev : {
-      user : 'node',
-      host : '212.83.163.1',
-      ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
-      path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-      env  : {
-        NODE_ENV: 'dev'
-      }
+  "deploy": {
+    "production": {
+      "user": "root",
+      "host": "jokcy.me",
+      "repo": "git@gitee.com:jokcylou/react-cnode-tech.git",
+      "ref": "origin/master",
+      "path": "/root/deploy/jnode-master",
+      "post-deploy": "npm install && npm run deploy && pm2 startOrRestart ecosystem.json --env production"
     }
   }
-};
+}
